51a95e217e17bc9400310a89494c708ae0f264fc
Wang Chong <mz1874@aliyun.com>
2025-07-05 15:58:52 +0800

    # reduce , filter , collect, Comparator

--- src/com/java/eight/fucntionalInterfaceAndLambda/lambda/RFilter.java
+++ src/com/java/eight/fucntionalInterfaceAndLambda/lambda/RFilter.java
@@ -1,7 +1,9 @@
 package com.java.eight.fucntionalInterfaceAndLambda.lambda;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -10,7 +12,7 @@ import java.util.stream.Stream;
  */
 public class RFilter {
     public static void main(String[] args) {
-        List<Integer> arr = Arrays.asList(1, 2, 3, 4, 5);
+        List<Integer> arr = Arrays.asList(1, 2, 3, 10, 4, 5, 2, 9, 3);
         //间接操作
         List<Integer> list = arr.stream().filter(i -> i > 3).toList();
 
@@ -22,6 +24,21 @@ public class RFilter {
         stream.forEach(System.out::println);
         list2.forEach(System.out::println);
 
+        //sorting
+        arr.sort(Comparator.comparingInt(a -> (int) a).reversed());
+        System.out.println(arr);
+
+
+        //从一个整数列表中过滤出偶数，并将它们平方后排序。
+        List<Integer> collect = arr.stream().filter(number -> number % 2 == 0)
+                .map(number -> number * number)
+                .sorted()
+                .collect(Collectors.toList());
+        System.out.println(collect);
+
+        //计算一个整数列表中所有元素的乘积。
+        Optional<Integer> reduce = arr.stream().reduce((a, b) -> a * b);
+        reduce.ifPresent(System.out::println);
 
     }
 }
