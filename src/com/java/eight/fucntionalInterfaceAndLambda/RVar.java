8c024169d5a152660164f55a1881abb4722e0978
WangChong <mz1874@aliyun.com>
2025-07-05 23:49:41 +0800

    # 完成Java 8 特性

---
+++ src/com/java/eight/fucntionalInterfaceAndLambda/RVar.java
@@ -0,0 +1,8 @@
+package com.java.eight.fucntionalInterfaceAndLambda;
+
+public class RVar {
+    public static void main(String[] args) {
+        var string = "Hello World";
+        System.out.println(string instanceof String);
+    }
+}
